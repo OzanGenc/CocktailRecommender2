e0ad392dac2872cc7c4a1e9df1f75cd1c88e32d1
OzanGenc <ozangenc35@hotmail.com>
2022-08-23 17:24:57 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
+import requests
 
 
 st.title("Cocktail Recommender 2.0!")
 
 user_input = None
+
+api_url = "https://wctppk9gul.execute-api.us-east-1.amazonaws.com/deploy_demo"
+
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 
+todo = {user_input}
+response = requests.put(api_url, json=todo)
+
 if user_input:
-  name = "Mai Tai"
-  st.markdown("**Given Cocktail is** {}".format(name))
+  st.markdown("**Given Cocktail is** {}".format(response))
 

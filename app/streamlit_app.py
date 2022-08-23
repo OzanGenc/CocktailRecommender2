add70643aac57dc1d87d0743900bf5cd76c8c8de
OzanGenc <ozangenc35@hotmail.com>
2022-08-23 16:50:21 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,8 +3,10 @@ import streamlit as st
 
 st.title("Cocktail Recommender 2.0!")
 
+user_input = None
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 
-name = "Mai Tai"
-st.markdown("**Given Cocktail is** {}".format(name))
+if user_input:
+  name = "Mai Tai"
+  st.markdown("**Given Cocktail is** {}".format(name))
 

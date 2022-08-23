4964b08d2d8da0902a3a7a6f0732d673314946b2
OzanGenc <ozangenc35@hotmail.com>
2022-08-23 18:07:45 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -12,7 +12,8 @@ user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 
 todo = user_input
-response = requests.put(api_url, json=todo)
+#response = requests.put(api_url, json=todo)
+response = requests.get(api_url, params=todo)
 
 if user_input:
   st.markdown("**Given Cocktail is** {}".format(response.json()))

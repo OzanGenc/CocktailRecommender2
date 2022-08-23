4b13fb06fab10f223b879c5fbe82135777170da4
OzanGenc <ozangenc35@hotmail.com>
2022-08-23 16:38:32 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,13 +3,3 @@ import streamlit as st
 
 st.title("Streamlit <-> Heroku Template App")
 
-
-# if button clicked, classify
-if st.button("Get Next Random Digit"):
-    # get random image from the test set
-    prediction = 5
-
-    # classify
-    
-    st.text("Predicted to be  : {}".format(prediction))
-    

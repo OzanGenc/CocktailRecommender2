a97e47cca0f016fe749abed1a4cce67ee59f4eec
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 11:49:56 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,14 +6,15 @@ st.title("Cocktail Recommender 2.0!")
 
 user_input = None
 
-api_url = "https://rhlxfhbjqj.execute-api.us-east-1.amazonaws.com/prod"
+api_url = " https://rhlxfhbjqj.execute-api.us-east-1.amazonaws.com/default/myLambdaDemo"
 
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 
 todo = user_input
 #response = requests.put(api_url, json=todo)
-response = requests.get(api_url, params=todo)
+#response = requests.get(api_url, params=todo)
+response = requests.request("POST", api_url, data=user_input)
 
 if user_input:
   st.markdown("**Given Cocktail is** {}".format(response.json()))

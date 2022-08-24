e6c83212af3936a4dcc9155b0b5dae198c8903a1
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 14:14:11 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -12,15 +12,14 @@ user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 
 #myobj = "vodka"
-myjob = user_input
-
-response = requests.post(api_url, json = myobj)
-
-response_ = response.json()["body"]
 
 #response = requests.request("POST", api_url, data=user_input)
 
 if user_input:
+  
+  myjob = user_input
+  response = requests.post(api_url, json = myobj)
+  response_ = response.json()["body"]
   st.markdown("**Given Cocktail3 is** {}".format(response_))
   #st.markdown("**Given Cocktail2 is** {}".format(response.json()))
   

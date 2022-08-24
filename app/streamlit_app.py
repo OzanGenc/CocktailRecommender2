f62b7efb3dfcef560afda4faea8d5edce29e3cd5
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 14:17:42 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -11,13 +11,8 @@ api_url = "https://wcdmx034sl.execute-api.us-east-1.amazonaws.com/prod/demo"
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 
-#myobj = "vodka"
-
-#response = requests.request("POST", api_url, data=user_input)
-
 if user_input:
   
-  myjob = user_input
   response = requests.post(api_url, json = user_input)
   response_ = response.json()["body"]
   st.markdown("**Given Cocktail3 is** {}".format(response_))

ac996131bd56f1c723053ab09b4f0d1ca662d56f
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 14:51:28 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,6 +2,31 @@ import streamlit as st
 import requests
 import os
 
+'''
+Follow instructions from here in order to create API Gateway. 
+
+https://shreyansh26.github.io/post/2022-01-23_model_deployment_using_aws_lambda/
+
+
+Create Lambda function from scratch. On Heroku reveal config vars insert api_url without "" (quotes).
+The Lambda function --> 
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    response = event + " hello from lambda_handler"
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
+
+
+
+'''
 
 st.title("Cocktail Recommender 2.0!")
 
@@ -16,7 +41,6 @@ user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 if user_input:
   
-  st.markdown("**API URL is** {}".format(api_url))
   response = requests.post(api_url, json = user_input)
   response_ = response.json()["body"]
   st.markdown("**Given Cocktail3 is** {}".format(response_))

6de63143e09ed4de713dbab3130499d37c7cc110
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 14:03:45 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,7 +6,7 @@ st.title("Cocktail Recommender 2.0!")
 
 user_input = None
 
-api_url = "https://wcdmx034sl.execute-api.us-east-1.amazonaws.com/prod"
+api_url = "https://wcdmx034sl.execute-api.us-east-1.amazonaws.com/prod/demo"
 
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 

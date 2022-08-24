813fede24eb92c345868261c09c8db9394c2d82c
OzanGenc <ozangenc35@hotmail.com>
2022-08-24 11:55:54 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -11,10 +11,9 @@ api_url = " https://rhlxfhbjqj.execute-api.us-east-1.amazonaws.com/default/myLam
 user_input = st.text_input(label="Please write a cocktail name.").upper()
 
 
-todo = user_input
-#response = requests.put(api_url, json=todo)
-#response = requests.get(api_url, params=todo)
-response = requests.request("POST", api_url, data=user_input)
+
+
+response = requests.request("POST", api_url, data={user_input})
 
 if user_input:
   st.markdown("**Given Cocktail is** {}".format(response.json()))

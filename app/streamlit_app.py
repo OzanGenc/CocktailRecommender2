b5d76aa172589343c06d2a87df43da0c7032d9a0
OzanGenc <ozangenc35@hotmail.com>
2022-08-28 16:17:40 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -35,7 +35,7 @@ if user_input:
     
     st.markdown("**Given Cocktail is** [{}]({})".format(user_input, df.loc[user_input]['link']))
     
-    st.markdown("**Recommended Cocktails are** )
+    st.markdown("**Recommended Cocktails are**" )
     
     st.markdown("**[{}]({}) - :cocktail: - [{}]({}) - :tropical_drink: - [{}]({}) - :wine_glass: - [{}]({}) - :beer: - [{}]({})**".format(
             recommended_cocktails.index[0], df.loc[recommended_cocktails.index[0]]['link'], 

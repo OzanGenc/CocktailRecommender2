8811d0346ceec4daf3a07b3002ab4d68c6fa5f57
OzanGenc <ozangenc35@hotmail.com>
2022-08-29 15:21:41 +0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -37,7 +37,7 @@ else:
 
 user_input = None
 
-user_input = st.text_input(label="Please write a cocktail name.").upper()
+user_input = st.text_input(label="Please write a cocktail name, ingredients, or a cocktail desciption.").upper()
 
 
 
